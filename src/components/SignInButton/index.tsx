dd2a4129460054a3705744f74aba1d3709d67b2c
igor_cerqueira_jussi <igor_cerqueira_jussi@whirlpool.com>
2022-07-12 16:41:48 -0300

    auth github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,24 @@
 import { FaGithub } from "react-icons/fa";
-import { FiX } from 'react-icons/fi';
+import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
+
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.button}>
-      <FaGithub color="#04d361" /> Igor Puto
-      <FiX color="#737380" className={styles.closeIcon} />
+  return session ? (
+    <button type="button" 
+      className={styles.button}>
+      <FaGithub color="#04d361" /> {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} onClick={() => signOut()} />
     </button>
   ) : (
-    <button type="button" className={styles.button}>
+    <button
+      type="button"
+      className={styles.button}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" /> Signin with github
     </button>
   );
