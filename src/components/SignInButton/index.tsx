d2fac8df2dd2e80901b5f23f63aa942c70394b29
igor_cerqueira_jussi <igor_cerqueira_jussi@whirlpool.com>
2022-07-12 15:37:39 -0300

    front - home

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,18 @@
+import { FaGithub } from "react-icons/fa";
+import { FiX } from 'react-icons/fi';
+import styles from "./styles.module.scss";
+
+export function SignInButton() {
+  const isUserLoggedIn = false;
+
+  return isUserLoggedIn ? (
+    <button type="button" className={styles.button}>
+      <FaGithub color="#04d361" /> Igor Puto
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="button" className={styles.button}>
+      <FaGithub color="#eba417" /> Signin with github
+    </button>
+  );
+}
